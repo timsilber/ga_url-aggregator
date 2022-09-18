bbfde6344c86a2029c2bedda8a1bcf02dc9a6cc4
tim silber <tim.silber@me.com>
2022-09-18 18:18:24 -0400

    Allow drag and drop, dynamic metric

--- url_aggregator.py
+++ url_aggregator.py
@@ -1,10 +1,15 @@
 import csv
 import pandas as pd
+from pathlib import Path
 
-path_to_import ='/Users/timsilber/repos/ga_url-aggregator/2022AUG_sessions.csv'
+path_to_import =input("\n Drop Google Analytics Report CSV here: ")
+metric = input("\n What metric do you want to sort by? ")
 
-col_names = ['Page', 'Sessions']
-csv_dict =  pd.read_csv(path_to_import, header= 5, usecols=col_names).to_dict(orient='split')
+metric = metric[0].upper() + metric[1:]
+print(metric)
+
+col_names = ['Page', metric]
+csv_dict =  pd.read_csv(path_to_import[1:-1], header= 5, usecols=col_names).to_dict(orient='split')
 csv_data = csv_dict['data']
 
 aggregate_dict = {}
@@ -23,12 +28,12 @@ for item in csv_data:
         else: 
             aggregate_dict[slug] = aggregate_dict[slug] + sessions
     except:
-        continue 
+        continue
 
 sorted_keys = sorted(aggregate_dict, key=aggregate_dict.get, reverse=True)
 
 csv_columns = ['Page','Sessions']
-output_file = "aggregated_sessions_per_page.csv"
+output_file = str(Path.home() / "Desktop") + "/aggregated_" + metric.lower() + "_per_page.csv"
 
 with open(output_file, 'w') as output_file:
     writer = csv.DictWriter(output_file, fieldnames=csv_columns)
@@ -36,7 +41,4 @@ with open(output_file, 'w') as output_file:
     for key in sorted_keys:
         output_file.write("%s,%s\n"%(key,aggregate_dict[key]))
 
-
-
-# for key, value in csv_dict.items():
-#      print(key, ' : ', value)
+print("\nDone. Check your Desktop.\n")
