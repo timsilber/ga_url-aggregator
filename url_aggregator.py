ec414cdd3798e1d8e10395a3f9c3ca2101a2b984
tim silber <tim.silber@me.com>
2022-09-18 15:44:23 -0400

    sort dict, write to csv

--- url_aggregator.py
+++ url_aggregator.py
@@ -3,11 +3,40 @@ import pandas as pd
 
 path_to_import ='/Users/timsilber/repos/ga_url-aggregator/2022AUG_sessions.csv'
 
-import_file =  pd.read_csv(path_to_import, index_col=0, header=7, squeeze=True).to_dict()
+col_names = ['Page', 'Sessions']
+csv_dict =  pd.read_csv(path_to_import, header= 5, usecols=col_names).to_dict(orient='split')
+csv_data = csv_dict['data']
 
-print(import_file)
+aggregate_dict = {}
 
-columns = ['Page, Sessions']
+for item in csv_data:
+    page = item[0]
+    try: 
+        sessions = int(item[1].replace(',', ''))
+    except:
+        continue
+    
+    try:
+        slug = page.split('/')[-1]
+        if slug not in aggregate_dict:
+            aggregate_dict[slug] = sessions
+        else: 
+            aggregate_dict[slug] = aggregate_dict[slug] + sessions
+    except:
+        continue 
+
+sorted_keys = sorted(aggregate_dict, key=aggregate_dict.get, reverse=True)
+
+csv_columns = ['Page','Sessions']
+output_file = "aggregated_sessions_per_page"
+
+with open(output_file, 'w') as output_file:
+    writer = csv.DictWriter(output_file, fieldnames=csv_columns)
+    writer.writeheader()
+    for key in sorted_keys:
+        output_file.write("%s,%s\n"%(key,aggregate_dict[key]))
 
 
-# pd.read_csv(path_to_import,usecols=columns).to_csv('selected.csv', index=False)
+
+# for key, value in csv_dict.items():
+#      print(key, ' : ', value)
