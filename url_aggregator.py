432eabdebda31523ab0fcca1a47af3a2cd1b89a8
tim silber <tim.silber@me.com>
2022-10-03 17:44:24 -0400

    Refactor into pure functions

--- url_aggregator.py
+++ url_aggregator.py
@@ -3,50 +3,69 @@ import pandas as pd
 from pathlib import Path
 import time 
 
-#get file path
-path_to_import =input("\n Drop Google Analytics Report CSV here: ")
-path_to_import = path_to_import.replace('\'', '').replace('"','') #handle terminal wrapping file paths in strings
-
-#get metric to sort pages
-metric = input("\n What metric do you want to sort by? ")
-metric = metric[0].upper() + metric[1:] # handle case-sensitive user input
-
-#choose column from Analytics CSV based on user input and turn into dictionary
-col_names = ['Page', metric]
-csv_dict =  pd.read_csv(path_to_import, header= 5, usecols=col_names).to_dict(orient='split')
-csv_data = csv_dict['data']
-
-aggregate_dict = {}
-
-for item in csv_data: 
-    page = item[0]
+def csv_to_dataframe(csv_path, columns):
+    #choose column from Analytics CSV based on user input and turn into dictionary
+    csv_dict =  pd.read_csv(csv_path, header= 5, usecols=columns).to_dict(orient='split')
+    csv_data = csv_dict['data']
     
-    #remove commas from numbers to turn into integers to perform operations in next try/except
-    try: 
-        sessions = int(item[1].replace(',', ''))
-    except:
-        continue
+    return csv_data
+
+def dataframe_to_dict(dataframe):
+
+    aggregate_dict = {}
+
+    for item in dataframe: 
+        page = item[0]
+        
+        #remove commas from numbers to turn into integers to perform operations in next try/except
+        try: 
+            sessions = int(item[1].replace(',', ''))
+        except:
+            continue
+        
+        #split URL by /, take the last item which should be slug that matches
+        try:
+            slug = page.split('/')[-1]
+            if slug not in aggregate_dict:
+                aggregate_dict[slug] = sessions
+            else: 
+                aggregate_dict[slug] = aggregate_dict[slug] + sessions #if matching slug, add metric to existing data
+        except:
+            continue
     
-    #split URL by /, take the last item which should be slug that matches
-    try:
-        slug = page.split('/')[-1]
-        if slug not in aggregate_dict:
-            aggregate_dict[slug] = sessions
-        else: 
-            aggregate_dict[slug] = aggregate_dict[slug] + sessions #if matching slug, add metric to existing data
-    except:
-        continue
+    return aggregate_dict
 
-#sort dictionary by pageviews
-sorted_keys = sorted(aggregate_dict, key=aggregate_dict.get, reverse=True)
-
-#write data to CSV output on Desktop
-csv_columns = ['Page', metric]
-output_file = str(Path.home() / "Downloads") + "/" + time.strftime('%Y-%m-%d') + "_aggregated_" + metric.lower() + "_per_page.csv"
-with open(output_file, 'w') as output_file:
-    writer = csv.DictWriter(output_file, fieldnames=csv_columns)
-    writer.writeheader()
+def sort_dict_by_value(dict):
+    #sort dictionary by pageviews
+    sorted_keys = sorted(dict, key=dict.get, reverse=True)
     for key in sorted_keys:
-        output_file.write("%s,%s\n"%(key,aggregate_dict[key]))
+        print(key, dict[key])
+    return sorted_keys
 
-print("\nDone. Check your Downloads folder.\n")
+def sorted_dict_to_csv(sorted_keys, dict, columns):
+    #write data to CSV output on Desktop
+    output_file = str(Path.home() / "Downloads") + "/" + time.strftime('%Y-%m-%d') + "_aggregated_" + columns[1].lower() + "_per_page.csv"
+    with open(output_file, 'w') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=columns)
+        writer.writeheader()
+        for key in sorted_keys:
+            output_file.write("%s,%s\n"%(key, dict[key]))
+
+    print("\nDone. Check your Downloads folder.\n")
+    
+if __name__ == "__main__":
+    #get file path
+    path_to_import =input("\n Drop Google Analytics Report CSV here: ")
+    path_to_import = path_to_import.replace('\'', '').replace('"','') #handle terminal wrapping file paths in strings
+
+    #get metric to sort pages
+    metric = input("\n What metric do you want to sort by? ")
+    metric = metric[0].upper() + metric[1:].lower() # handle case-sensitive user input
+    col_names = ['Page', metric]
+
+    df = csv_to_dataframe(path_to_import, col_names)
+    csv_dict = dataframe_to_dict(df)
+    sorted_dict = sort_dict_by_value(csv_dict)
+
+    sorted_dict_to_csv(sorted_dict, csv_dict, col_names)
+    
